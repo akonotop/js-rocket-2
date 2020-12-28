0ae766c6dde834457e1ffe4a5442ee8c6338f611
Alex Konotop <38548876+akonotop@users.noreply.github.com>
2020-12-28 10:33:06 +0700

    added styles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Hello friends!!!");
+
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+	if(currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+}
